1736dd329d918b5e05b81fd332e50b7abc3669df
shykulislamsiam <86303778+shykulislamsiam@users.noreply.github.com>
2022-04-01 21:00:59 +0600

    Add files via upload

---
+++ exercise -chapter-3-math-1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 20:40:05 2022
+
+@author: shyku
+"""
+
+number1 = input("Please type an integer and press enter: ")
+number2 = input("Please type another integer and press enter: ")
+number1 = int(number1)
+number2 = int(number2)
+result = number1 + number2
+print("Result:",result)
+result = number1 - number2
+print("Result:",result)
+result = number1 * number2
+print("Result:",result)
+result = number1 / number2
+print("Result:",result)
+result = number1 % number2
+print("Result:",result)
+result = number1 // number2
+print("Result:",result)
+result = number1 ** number2
+print("Result:",result)
+
+
+
