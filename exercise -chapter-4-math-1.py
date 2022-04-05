04bd96e83b5cf93cf956af246c84064ae734e1f5
shykulislamsiam <86303778+shykulislamsiam@users.noreply.github.com>
2022-04-05 19:37:16 +0600

    Add files via upload

---
+++ exercise -chapter-4-math-1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 19:13:19 2022
+
+@author: shykul
+"""
+
+number = input("Please enter the number:")
+number = int(number)
+if number > 0:
+    print(number,"is postive")
+else:
+    print(number,"is negative")
