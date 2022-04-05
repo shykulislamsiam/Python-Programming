289d789e36c2ee4282c5319c746817c54260fd05
shykulislamsiam <86303778+shykulislamsiam@users.noreply.github.com>
2022-04-05 19:37:37 +0600

    Add files via upload

---
+++ exercise -chapter-4-math-2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 19:17:50 2022
+
+@author: shykul
+"""
+
+N = input("Please enter an integer number:")
+N = int(N)
+if N%2==0:
+    print(N,"is an even number")
+else:
+   print(N,"is an odd number")    
