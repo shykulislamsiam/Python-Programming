dbe64b156cc1b165aa014ec4b82da996defcd276
shykulislamsiam <86303778+shykulislamsiam@users.noreply.github.com>
2022-04-05 21:58:48 +0600

    Add files via upload

---
+++ exercise -chapter-5-math-1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 21:34:55 2022
+
+@author: shykul
+"""
+
+import turtle
+turtle.speed(2)
+turtle.forward(150)
+turtle.left(120)
+turtle.forward(150)
+turtle.left(120)
+turtle.forward(150)
+turtle.left(120)
+turtle.exitonclick()
