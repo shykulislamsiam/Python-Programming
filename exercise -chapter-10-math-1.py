380a3252e277bf13a233078c0c625f5a8d7c4238
shykulislamsiam <86303778+shykulislamsiam@users.noreply.github.com>
2022-04-19 22:49:41 +0600

    Add files via upload

---
+++ exercise -chapter-10-math-1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 21:44:25 2022
+
+@author: shyku
+"""
+
+start = int (input("Enter the starting range:"))
+end = int (input("Enter the end range: "))
+
+print ("Prime numbers in the range", start, "to", end)
+
+for i in range(start, end+1):
+    flag = 0
+    for j in range(2, i):
+        if (i % j == 0):
+            flag = 1
+            break
+            
+    if (flag == 0):
+        print (i, end = '\t')
